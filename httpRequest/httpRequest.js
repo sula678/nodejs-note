2733f97929455b2f52b3a6efe649f20c46460d84
Neil Tu <sula678@gmail.com>
2014-09-03 16:53:16 +0900

    Update httpRequest.js

--- httpRequest/httpRequest.js
+++ httpRequest/httpRequest.js
@@ -12,7 +12,7 @@ var req = http.request(options, function(res) {
   console.log('HEADERS: ' + JSON.stringify(res.headers));
   res.setEncoding('utf8');
   res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
+    console.log(chunk);
   });
 });
 
@@ -21,6 +21,6 @@ req.on('error', function(e) {
 });
 
 // write data to request body
-req.write('data\n');
-req.write('data\n');
+//req.write('data\n');
+//req.write('data\n');
 req.end();
